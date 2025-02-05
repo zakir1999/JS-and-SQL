bbc99a0dc5200939d4c1d0a4c2bd4de4aea9e975
zakir1999 <m.zakirhossen99@gmail.com>
2025-02-05 11:15:07 +0600

    all about map

---
+++ fifth.js
@@ -0,0 +1,4 @@
+const store=new Map([['Apple',10],['Banana',20],['Orange',30],['Mango',40],['Pineapple',50],['Grapes',60],['Guava',70],['Papaya',80],['Watermelon',90],['Pomegranate',100]]);
+for(let x of store){
+    console.log(x);
+}
--- forth.js
+++ forth.js
@@ -1,3 +1,21 @@
-for(let i=1;i<100;i++){
-    i%2==0?console.log(i):null;
+const nums=[1,2,3,4,5];
+let txt="";    
+nums.forEach(fun); 
+function fun(value){
+    txt+=value+"<br>";
+}
+document.getElementById("demo").innerHTML=txt;
+
+
+let str="I am a student of computer science and enginnering";
+// for(let x of str){
+//     document.getElementById("demo").innerHTML+=x+"<br>";
+// }
+
+const letters= new Set();
+for (let x of str){
+    letters.add(x);
+}
+for(let x of letters.entries()){
+    console.log(x);
 }
