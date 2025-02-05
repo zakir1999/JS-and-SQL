ca7720a3f6f869e350c9ba47173b9258389d81fc
zakir1999 <m.zakirhossen99@gmail.com>
2025-02-05 12:04:23 +0600

    arrow function

--- fifth.js
+++ fifth.js
@@ -1,4 +1,15 @@
 const store=new Map([['Apple',10],['Banana',20],['Orange',30],['Mango',40],['Pineapple',50],['Grapes',60],['Guava',70],['Papaya',80],['Watermelon',90],['Pomegranate',100]]);
-for(let x of store){
-    console.log(x);
+// for(let x of store){
+//     console.log(x);
+// }
+store.set('Strawberry',110);
+// for(let x of store){
+//     console.log(x);
+// }
+
+function sum(a,b){
+    return a+b;
 }
+const arrowsum=(a,b)=>{
+    console.log(a+b);
+};
